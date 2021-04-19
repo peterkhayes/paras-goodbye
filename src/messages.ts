824e64d68c23d502fcdbd0fe51ee589c6eedecb8
Peter Hayes <peter@remix.com>
2021-04-19 10:28:04 -0700

    more message

--- src/messages.ts
+++ src/messages.ts
@@ -1,4 +1,8 @@
 const MESSAGES: Record<string, string> = {
+  Alyssa:
+    "I'm so happy to have met you! You have done so many incredible things for Remix, I probably only know the tip of the iceberg! I also really liked seeing you in #pizza and #woo-music, I'll miss you.",
+  Brooke:
+    "Farewell, Paras! Thank you for all of your leadership on the Transit team. (Now that you're leaving, we were considering introducing this new thing into the code base called Ampersand?) Anyways hope you have a relaxing break and get to do a bike tour or something exciting!",
   Cara: "Your light will be missed!!",
   Caroline:
     "Paras - thank you for everything you brought to Remix as a leader and an engineer. Wishing you all the best for your next chapter!",
@@ -7,23 +11,34 @@ const MESSAGES: Record<string, string> = {
   Charlie:
     "Your drive to make the world as you want it to be is infectious. I know you'll bring that to wherever you end up next, but I'll miss it here. Glad we had the chance to work together!",
   Dan:
-    "Paras - I'll eat an eggplant parm in your honor. Seriously though, I've learned so much from working with you, and will always treasure that. ",
+    "Paras - I'll eat an eggplant parm in your honor. Seriously though, I've learned so much from working with you, and will always treasure that.",
+  "David M":
+    "Paras, thank you for being such a great collaborator; we will greatly miss your kindness, technical skills, and your empathy. I hope we get to cross paths again.",
   Dmitriy:
     "we've had a good run. thanks for caring about the right stuff and always elevating voices in your team. please continue sending me neat music though thx",
   Drew:
     "Wishing you all the best on your next adventure. May the zahhhh parties continue where ever you go!! 🍕🍕",
+  Gories:
+    "PARAS! I remember when I first started at Remix, you were so warm and welcoming and chilllll, and then we got talking about music and festivals and I was like damn... this guy is cool. You brought such a special vibe to work every day and I'm gonna miss it and you! Best of luck on your next zaaaaventure buddy 🍕 and I hope to see you groovin' around sometime at a show soon, once it's safe :) Take care!",
   Isaac:
     "It's been such a pleasure working with and learning from you. You bring such a contagiously positive energy to everything you do. See ya around!",
   Kate:
     "Paras, thank you so much for being an important, irreplaceable, genuine part of culture at Remix.",
   Kelsey:
-    "We will miss you, Paras! Hope to cross paths again in the future. :) ",
+    "We will miss you, Paras! Hope to cross paths again in the future. :)",
+  Matthew:
+    "Paras - I'm so glad we got to share one of my favorite slices when you visited for MTA workshops! You've been a wonderful steward of Transit and am grateful to have worked with such a passionate and principled colleague. Looking forward to meeting up the next time you're home in NY :)",
   Loren:
     "Paras! I've felt your impact here in three ways: 1. great music recommendations, 2. the kind closeness of the transit team, 3. the joy I got from hanging out in person and over zoom. You'll be missed, good luck on the new adventure",
   Peter:
     "It has been an absolute time working with you these last couple of years. I've loved having someone I truly respect and trust as a peer as we both transitioned into managers. Couldn't have done it without you. Wishing you all the best.",
   Priya:
     "Paras, thank you for helping make Remix a place where folks can learn, grow, and be themselves. You've contributed greatly to the sense of community and belonging at Remix, and you will be missed!",
+  Qiqi:
+    "Sad that the transit team is losing an important slice of 🍕 ! Although we've only worked together for a way too brief amount of time, I've truly appreciate your authenticity, thoughtfulness, and your care for the team. Wishing you the very best for whatever you choose to do next - you will be missed! ",
+  Sachin: "Let's meet up at Zante's and try some Indian pizza!",
+  Sarah:
+    "It has been such a pleasure working with you over the last couple of years.  In particular I am so grateful that I had the opportunity to partner with you as team leads for the Transit team.  I have learned so much from you and know I am a better PM thanks to your feedback and teamwork.  Your impact on this team is strongly felt: your EM leadership has resulted in a team that prioritizes pairing and discourages heroics.  Thank you so much for everything you have brought to this team and best of luck with your next adventure.",
   "Stuart C":
     "Paras, you've had a huge impact on the team. It's been a true pleasure working with you and I sincerely hope we can do it again someday. I'll miss you and I'll see you around (KGATLW in October?!?)",
   Tiffany:
